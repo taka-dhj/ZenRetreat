0fc04a17afdaaa5e769f0da81427fe4ea8684722
taka-dhj <taka@discoveryhiddenjapan.com>
2025-12-10 13:34:18 +0800

    photos

--- src/lib/imageUtils.ts
+++ src/lib/imageUtils.ts
@@ -1,6 +1,11 @@
 const imageMap: Record<string, string> = {
   '/cebu2.jpg': 'https://images.pexels.com/photos/457882/pexels-photo-457882.jpeg?auto=compress&cs=tinysrgb&w=1200',
   '/cebu-mountain.jpg': 'https://images.pexels.com/photos/3225531/pexels-photo-3225531.jpeg?auto=compress&cs=tinysrgb&w=1200',
+  // Tokyo Kiyosumi Zen Retreat images
+  '/tokyo-kiyosumi-garden.jpg.JPG': '/tokyo-kiyosumi-garden.jpg.JPG',
+  '/Kiyosumiteiein.jpg': '/Kiyosumiteiein.jpg',
+  '/Reiganji.jpg': '/Reiganji.jpg',
+  '/Kintsugi sample.jpg': '/Kintsugi sample.jpg',
 };
 
 export const getImageUrl = (imagePath: string | undefined | null): string => {
